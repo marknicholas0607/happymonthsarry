c8cd3be55c4e7c0a098ea56b367f8258f5178cc6
Mark Nicholas Razon <95486816+marknicholas0607@users.noreply.github.com>
2022-08-01 20:43:00 +0800

    Add files via upload

---
+++ features.js
@@ -0,0 +1,50 @@
+function showFirst(argument) {
+	var x = document.getElementById('modal2');
+
+	if (x.style.height === "100%") {
+		x.style.height = "0";
+	}
+	else{
+		x.style.height = "100%";
+	}
+}
+function showSecond(argument) {
+	var x = document.getElementById('modal3');
+
+	if (x.style.height === "100%") {
+		x.style.height = "0";
+	}
+	else{
+		x.style.height = "100%";
+	}
+}
+function showThird(argument) {
+	var x = document.getElementById('modal4');
+
+	if (x.style.height === "100%") {
+		x.style.height = "0";
+	}
+	else{
+		x.style.height = "100%";
+	}
+}
+function showForth(argument) {
+	var x = document.getElementById('modal5');
+
+	if (x.style.height === "100%") {
+		x.style.height = "0";
+	}
+	else{
+		x.style.height = "100%";
+	}
+}
+function showFifth(argument) {
+	var x = document.getElementById('modal6');
+
+	if (x.style.height === "100%") {
+		x.style.height = "0";
+	}
+	else{
+		x.style.height = "100%";
+	}
+}
